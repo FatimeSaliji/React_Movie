4d5b8a81d5622703348a3219a9c7ab298e204d55
FatimeSaliji <fatime_saliji@hotmail.com>
2024-02-01 23:36:25 +0100

    react

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,71 +1,71 @@
 import React from 'react'
 import  {useEffect, useState} from 'react'
-import Pagination from '../components/Pagination';
+
 
 
 
 export default function Movie() {
   
    
-    const [movies, setMovies] = useState([])
-    
-     useEffect(() => {
-       const fetchData = async () => {
-           const api_url = 'https://api.themoviedb.org/3/discover/movie?api_key=3e52e2f5350ae60de5e2fc58e818d2a0'; 
-   
-           try {
-               const response = await fetch(api_url);
-               const data = await response.json();
-               const moviesData = data.results;
-               setMovies(moviesData);
-           } catch (error) {
-               console.error('Error fetching data:', error);
-           }
-       };
-   
-       fetchData()
-   }); 
-   
-     return (
-       <>
-        <div className='container'>
-        <div className='row'>
-        
-        
-                   {movies.map((movie) => (
-                     <div className='col-3 p-3 g-col-6'>
-                       <div key={movie.id} className="card" style={{ width: '18rem' }}>
-                           <img
-                               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                               className="card-img-top" style={{ height: '230px' }}
-                               alt="Movies"
-                           />
-                           <div className="card-body">
-                               <h5 className="card-title fw-bold" style={{ height: '40px' }}>{movie.title}</h5>
-                               
-                               <p className="mb-4 fw-bold">
-                    <i className="bi bi-calendar-check">&nbsp;&nbsp;{movie.release_date}</i> 
+    const [totalPages, setTotalPages] = useState(1);
+    const [page, setPage] = useState(1);
+  
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
+  
+    useEffect(() => {
+      const getFetch = () => {
+        setLoading(true);
+        fetch(
+          `https://api.themoviedb.org/3/discover/movie?api_key=3e52e2f5350ae60de5e2fc58e818d2a0&page=${page}`
+        )
+          .then((res) => res.json())
+          .then((res) => {
+            setTotalPages(res.total_pages);
+            setMovies([ ...movies,...res.results]);
+            setLoading(false);
+          });
+      };
+      getFetch();
+    }, [page]);
+  
+    return (
+      <div className="container">
+        <div className="row">
+          {movies.map((movie, i) => (
+              <div className='col-3 p-3'>
+             <div key={i.id} className="box " style={{ width: '18rem' }} >
+            
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                  className="card-img-top"
+                  style={{ height: '230px' }}
+                  alt="Movies"
+                />
+                <div className="card-body ">
+                  <h5 className="card-title fw-bold" style={{ height: '40px' }}>
+                    {movie.title}
+                  </h5>
+                  <p className="mb-4 fw-bold">
+                    <i className="bi bi-calendar-check ">
+                      &nbsp;&nbsp;{movie.release_date}
+                    </i>
                     <br />
-                    <i className="bi bi-star">&nbsp;&nbsp;{movie.vote_average}</i> 
-                    
-      
-                </p>
-                               <a href="/Movie" className="btn btn-outline-primary">
-                               <i className="bi bi-arrow-right"></i>
-                               </a>
-                           </div>
-                       </div>
-                       </div>
-                   ))}
-               </div>
-               <Pagination/>
-           </div>
-           
-       </>
-       
-     )
-   
-  
-  
-}
-
+                    <i className="bi bi-star">&nbsp;&nbsp;{movie.vote_average}</i>
+                  </p>
+                  <a href="/Movie" className="btn btn-outline-primary">
+                    <i className="bi bi-arrow-right"></i>
+                  </a>
+                </div>
+              </div>
+              </div>
+          ))} 
+        </div>
+        {totalPages !== page && (
+          <button className="btn btn-outline-primary  d-grid gap-2 col mt-4 mb-4 mx-auto" onClick={() => setPage(page + 1)}>
+            {loading ? 'Loading...' : 'Load More'}
+          </button>
+        )}
+      </div>
+    );
+  }
